3c01d5d031a42c92e91bd4f15d1316d056666b34
205113 <205113@lepcopi11.leplab.polito.it>
2016-03-16 11:17:36 +0100

    completo fino a grafica

--- Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,49 @@
 package it.polito.tdp.spellchecker.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class SpellCheckerController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<String> lingua;
+
+    @FXML
+    private TextArea areatesto;
+
+    @FXML
+    private Button controlla;
+
+    @FXML
+    private TextArea areaerrori;
+
+    @FXML
+    private Label errore;
+
+    @FXML
+    private Button clear;
+
+    @FXML
+    void initialize() {
+        assert lingua != null : "fx:id=\"lingua\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert areatesto != null : "fx:id=\"areatesto\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert controlla != null : "fx:id=\"controlla\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert areaerrori != null : "fx:id=\"areaerrori\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert errore != null : "fx:id=\"errore\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert clear != null : "fx:id=\"clear\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        lingua.getItems().addAll("English","Italiano");
+    }
 }
+
+
